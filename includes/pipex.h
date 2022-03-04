acf80b53f3b825b39a2967955a5a41ff42119416
Débora Oliveira <98doliveira@gmail.com>
2022-03-04 17:44:48 -0300

    initial commit to pipex

---
+++ includes/pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 10:06:02 by dde-oliv          #+#    #+#             */
+/*   Updated: 2022/03/04 17:40:42 by dde-oliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <string.h>
+# include <errno.h>
+# include "../.dependencies/printf/ft_printf.h"
+# include "childs.h"
+# include "structures.h"
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 09:32:12 by dde-oliv          #+#    #+#             */
+/*   Updated: 2022/03/04 17:42:28 by dde-oliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+void	close_files(t_pipex *pipex)
+{
+	close(pipex->infile);
+	close(pipex->outfile);
+	close(pipex->fd[0]);
+	close(pipex->fd[1]);
+}
+
+int	pipex_init(int argc, char **argv, t_pipex *pipex)
+{
+	if (argc != 5)
+	{
+		ft_printf("Input error: not enough arguments\n");
+		return (-1);
+	}
+	pipex->infile = open(argv[1], O_RDONLY);
+	if (pipex->infile < 0)
+	{
+		perror(argv[1]);
+		return (-2);
+	}
+	pipex->outfile = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0644);
+	if (pipex->outfile < 0)
+	{
+		perror(argv[argc - 1]);
+		return (-2);
+	}
+	if (pipe(pipex->fd) == -1)
+	{
+		perror("");
+		return (-3);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_pipex	pipex;
+
+	if (pipex_init(argc, argv, &pipex) != 0)
+		return (-1);
+	pipex.pid1 = fork();
+	if (pipex.pid1 < 0)
+	{
+		perror("");
+		return (-4);
+	}
+	if (pipex.pid1 == 0)
+		first_child(pipex, argv);
+	pipex.pid2 = fork();
+	if (pipex.pid2 < 0)
+	{
+		perror("");
+		return (-5);
+	}
+	if (pipex.pid2 == 0)
+		second_child(pipex, argv);
+	close_files(&pipex);
+	waitpid(pipex.pid1, NULL, 0);
+	waitpid(pipex.pid2, NULL, 0);
+}
